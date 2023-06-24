0f7866b42a3e22d48d5621e733c48a15cb6f1f69
Nikolai <dolinnikolai@gmail.com>
2023-06-24 12:58:23 +0300

    task2

---
+++ task2.java
@@ -0,0 +1,23 @@
+
+/**
+ * Task#1
+ */
+// Вывести все простые числа от 1 до 1000
+
+public class task2 {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 1000; i++) {
+            boolean flag = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                System.out.println(i);
+            }
+        }
+    }
+}
